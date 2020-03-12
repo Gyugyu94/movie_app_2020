74b929e49697e2222d5d4766206719a466774421
rbalsdl815 <rbalsdl1501@gmail.com>
2020-03-12 17:48:55 +0900

    Updates

---
+++ precache-manifest.aa555552fd9a95305481d24111c8cc85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c16a06a31692ee8377ad4e846ac6802",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a967aa95428bb187413b",
+    "url": "/movie_app_2020/static/css/main.8b1c71eb.chunk.css"
+  },
+  {
+    "revision": "885803aeff8d1d112a4c",
+    "url": "/movie_app_2020/static/js/2.6616bc11.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.6616bc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a967aa95428bb187413b",
+    "url": "/movie_app_2020/static/js/main.f5bd618c.chunk.js"
+  },
+  {
+    "revision": "dc83d77430231124db2f",
+    "url": "/movie_app_2020/static/js/runtime-main.8bdd8803.js"
+  }
+]);
